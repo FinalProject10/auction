405e646d922b968867889d525b2842b67e7236f0
salimBenSlim1 <salimbenslim09@gmail.com>
2024-01-18 10:20:12 +0100

    fix

--- back/routes/itemsRoute.js
+++ back/routes/itemsRoute.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const itemsController = require("../controllers/itemsController");
 
-const router = express.Router();
-
-router.get("/fetch-items/:itemId", itemsController.getItems);
-
+const Itemsrouter = express.Router();
 
 Itemsrouter.get("/fetch-items/:itemId", itemsController.getItems);
 Itemsrouter.get('/getAll',itemsController.getAllItems)
-Itemsrouter.post('/add',itemsController.addItem)module.exports = router;
+Itemsrouter.post('/add',itemsController.addItem)
+module.exports=Itemsrouter
