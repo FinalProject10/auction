46791983c527329b86e047fd601079f90372fd45
salimBenSlim1 <salimbenslim09@gmail.com>
2024-01-25 18:41:59 +0100

    client dash

--- back/index.js
+++ back/index.js
@@ -4,7 +4,7 @@ const stripe = require("stripe")(
   "sk_test_51Oa23kFgyHOf8MRLBiQ7NHVMbtwjQadZr4dQEePKGWzkjL5y1xpBDSD7COvLpuLiTXe5LQe3GUuQlEp7aF4Qf76l009Im1ojcX"
 );
 
-const express = require("express");
+const express = require("express")
 const db = require("./database/index");
 const {
   Bid,
@@ -15,7 +15,6 @@ const {
   Seller,
   Reclamation,
 } = require("./models/relations");
-const sellersRoutes = require("./routes/seller");
 const clientRoutes = require("./routes/client");
 const adminRoutes = require("./routes/admin");
 const dashboard = require("./routes/AdminDashboardRouter");
@@ -29,6 +28,7 @@ const bidRouter = require("./routes/bidRouter");
 
 const app = express();
 const userSocketMap = new Map();
+console.log('userScoket',userSocketMap)
 const corsOptions = {
   origin: function (origin, callback) {
     // console.log(origin);
